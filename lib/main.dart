94e46198427c17b363b629502ab324a157204a87
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-03-03 16:43:58 +0000

    Fixing Commit to Main

--- lib/main.dart
+++ lib/main.dart
@@ -90,6 +90,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
       title: 'ToDo',
       localizationsDelegates: [
         GlobalMaterialLocalizations.delegate,
